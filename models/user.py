235c3572585be91404afa613134d82977a7590d3
sebasSS88 <jaimesebas66@gmail.com>
2021-12-16 12:12:14 +0100

    User Odoo Data Models Modify

--- models/user.py
+++ models/user.py
@@ -6,6 +6,6 @@ class user(models.Model):
     _name = 'res.users'
     _inhertit = 'res.users'
     
-    privilege = fields.Char()
-    status = fields.Boolean()
+    privilege = fields.Selection([('0','USER'),('1','ADMIN')], string='Privilege', default='0')
+    status = fields.Boolean(default='True')
     lastPasswordChange = fields.Char()
