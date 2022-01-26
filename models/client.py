08ab8dec02a721ec3d4c1f7f19cb26a0f9c810f5
sebasSS88 <jaimesebas66@gmail.com>
2022-01-26 14:15:48 +0100

    - In the client model, change the enumeration named type, to tipo - In the client view change all the fields with the word type for the word tipo
    Type is a reserved word and it gave an error when loading it

--- models/client.py
+++ models/client.py
@@ -1,12 +1,17 @@
 # -*- coding: utf-8 -*-
 
-from odoo import models, fields
+from odoo import fields
+from odoo import models
 
 class client(models.Model):
     _name = 'rentalg1c.client'
     _inherit = 'res.users'
 
-    type = fields.Selection([('0','PARTICULAR'),('1','ASOCIATION'),('2','ENTERPRISE'),('3','PUBLIC_ENTITY')], string='Type', default='0')
+    tipo = fields.Selection([('0', 'PARTICULAR'),
+                            ('1', 'ASOCIATION'),
+                            ('2', 'ENTERPRISE'),
+                            ('3', 'PUBLIC_ENTITY')
+                            ], string='Tipo', copy='False')
 
     event = fields.One2many('rentalg1c.event', 'client', ondelete='set null', string='Event')
     commercial = fields.Many2one('rentalg1c.commercial', ondelete='set null', string='Commercial')
