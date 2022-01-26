449ced689c0c79aa61f35209413bbefb1a9256fe
andonialday <83644755+andonialday@users.noreply.github.com>
2022-01-26 16:08:05 +0100

    Event and EventEquipment Views(Pending "Cuter" design)+Search+Validations

--- models/event.py
+++ models/event.py
@@ -1,6 +1,9 @@
 # -*- coding: utf-8 -*-
 
-from odoo import models, fields, api
+from odoo import api
+from odoo import fields
+from odoo import models
+from odoo.exceptions import ValidationError
 
 class event(models.Model):
     _name = 'rentalg1c.event'
@@ -12,3 +15,9 @@ class event(models.Model):
     
     client = fields.Many2one('rentalg1c.client', ondelete='set null', string="Client")
     event_equipments = fields.One2many('rentalg1c.event_equipment', 'event', ondelete='set null', string="Rental")
+    
+    @api.constrains('dateEnd', 'dateStart')
+    def _verify_valid_end_date(self):
+        for record in self:
+            if record.dateEnd < record.dateStart:
+                raise ValidationError("The Event cannot have an Ending Date earlier than the Starting Date")
--- models/event_equipment.py
+++ models/event_equipment.py
@@ -1,11 +1,20 @@
 # -*- coding: utf-8 -*-
 
-from odoo import models, fields, api
+from odoo import api
+from odoo import fields
+from odoo import models
+from odoo.exceptions import ValidationError
 
 class event_equipment(models.Model):
     _name = 'rentalg1c.event_equipment'
     
-    quantity = fields.Integer(string = "Quantity", required = True)
+    quantity = fields.Integer(string="Quantity", required=True)
     
     event = fields.Many2one('rentalg1c.event', ondelete='set null', string="Event")
     equipment = fields.Many2one('rentalg1c.equipment', ondelete='set null', string="Equipment")
+
+    @api.constrains('quantity')
+    def _verify_positive_quantity(self):
+        for record in self:
+            if record.quantity < 1:
+                raise ValidationError("The Equipment Quantity rented for an Event cannot be negative (In case of 0, the entry would not have sense for being recorded)")
