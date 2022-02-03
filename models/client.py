c6c4821f0fa87cb3c1190fa60aef1a8a96578789
andonialday <83644755+andonialday@users.noreply.github.com>
2022-02-03 22:16:19 +0100

    Installs in 6 different Database, ready to deliver

--- models/client.py
+++ models/client.py
@@ -2,6 +2,8 @@
 
 from odoo import fields
 from odoo import models
+from odoo import api
+from odoo.exceptions import ValidationError
 
 class client(models.Model):
     _name = 'rentalg1c.client'
@@ -11,3 +13,19 @@ class client(models.Model):
 
     event = fields.One2many('rentalg1c.event', 'client', ondelete='set null', string='Event')
     commercial = fields.Many2one('rentalg1c.commercial', ondelete='set null', string='Commercial')
+    
+    @api.constrains('login')
+    def _vali_null_login(self):
+        if len(str(self.login)) > 25:
+            raise ValidationError("El campo login no puede tener mÃ¡s de 25 caracteres")
+          
+    
+    @api.onchange('name')
+    def _verify_valid_name(self):
+        if len(str(self.name)) > 25:
+            return {
+                'warning': {
+                    'title': "Error",
+                    'message': "El nombre del cliente no puede superar los 50 caracteres",
+                },
+            }
--- models/commercial.py
+++ models/commercial.py
@@ -4,20 +4,36 @@
 
 # -*- coding: utf-8 -*-
 
-from odoo import models, fields
+from odoo import fields
+from odoo import models
+from odoo import api
+from odoo.exceptions import ValidationError
 
 class commercial(models.Model):
-     _name = 'rentalg1c.commercial'
-     _inherit = 'res.users'
+    _name = 'rentalg1c.commercial'
+    _inherit = 'rentalg1c.usr'
      
-     especialization = fields.Selection([('0','SONIDO'),
-                                         ('1','ILUMINACION'),
-                                         ('2','PIROTECNIA'),
-                                         ('3','LOGISTICA')
-                                        ],string='Especializacion', copy='False' )
+    especialization = fields.Selection([('0', 'SONIDO'),
+                                       ('1', 'ILUMINACION'),
+                                       ('2', 'PIROTECNIA'),
+                                       ('3', 'LOGISTICA')
+                                       ], string='Especializacion', copy='False')
 
 
 
-     client = fields.One2many('rentalg1c.client','commercial', ondelete='set null', string="Client")
+    client = fields.One2many('rentalg1c.client', 'commercial', ondelete='set null', string="Client")
      
-  
+    @api.constrains('email')
+    def _vali_null_email(self):
+        if len(str(self.email)) < 10:
+            raise ValidationError("No existen emails validos tan cortos, revisalo")
+            
+    @api.onchange('name')
+    def _verify_valid_name(self):
+        if len(str(self.name)) > 25:
+            return {
+                'warning': {
+                    'title': "Error",
+                    'message': "El nombre del cliente no puede superar los 50 caracteres",
+                },
+            }
